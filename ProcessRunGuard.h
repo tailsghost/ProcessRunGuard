b43db4d7f3709d442ffc0a859195e07913f322a5
AndreyXP <tailsghost@vk.com>
2025-09-05 15:22:53 +0300

    update

--- ProcessRunGuard.h
+++ ProcessRunGuard.h
@@ -19,22 +19,7 @@ class ProcessRunGuard {
 public:
 	ProcessRunGuard(std::atomic<HANDLE>& currentProcess, std::mutex& processMutex, std::atomic<bool>& cancelRequested) : g_currentProcess(currentProcess), g_processMutex(processMutex), g_cancelRequested(cancelRequested) {}
 	~ProcessRunGuard() {
-		if (_current.pi.hProcess && !closeHProcess) {
-			CloseHandle(_current.pi.hProcess);
-			closeHProcess = true;
-		}
-		if (_current.pi.hThread && !closeHThread) {
-			CloseHandle(_current.pi.hThread);
-			closeHThread = true;
-		}
-		if (_current.out_read && !closeOutRead) {
-			CloseHandle(_current.out_read);
-			closeOutRead = true;
-		}
-		if (_current.err_read && !closeErrRead) {
-			CloseHandle(_current.err_read);
-			closeErrRead = true;
-		}
+		CloseHandlers();
 	}
 	ProcessRunGuardResult& StartProcessWithRedirect(const std::wstring& commandLine, const std::wstring& workDir) {
 		SECURITY_ATTRIBUTES sa{ sizeof(sa), nullptr, TRUE };
@@ -42,6 +27,7 @@ public:
 		HANDLE errRead = nullptr, errWrite = nullptr;
 
 		if (!CreatePipe(&outRead, &outWrite, &sa, 0)) {
+			CloseHandlers();
 			_current.last_error = GetLastError();
 			_current.message = L"Failed to create stdout pipe";
 			_current.seccess = false;
@@ -51,8 +37,7 @@ public:
 		SetHandleInformation(outRead, HANDLE_FLAG_INHERIT, 0);
 
 		if (!CreatePipe(&errRead, &errWrite, &sa, 0)) {
-			CloseHandle(outRead);
-			CloseHandle(outWrite);
+			CloseHandlers();
 			_current.last_error = GetLastError();
 			_current.message = L"Failed to create stderr pipe";
 			_current.seccess = false;
@@ -74,18 +59,13 @@ public:
 		auto ok = CreateProcessW(nullptr, cmdBuf, nullptr, nullptr, TRUE, CREATE_NO_WINDOW, nullptr, workDir.empty() ? nullptr : (LPWSTR)workDir.c_str(), &si, &pi);
 
 		if (!ok) {
-			CloseHandle(outRead);
-			CloseHandle(outWrite);
-			CloseHandle(errRead);
-			CloseHandle(errWrite);
+			CloseHandlers();
 			_current.last_error = GetLastError();
 			_current.message = L"CreateProcess failed";
 			_current.seccess = false;
 			return _current;
 		}
 
-		CloseHandle(outWrite);
-		CloseHandle(errWrite);
 
 		std::lock_guard<std::mutex> lk(g_processMutex);
 		g_currentProcess = pi.hProcess;
@@ -107,4 +87,23 @@ private:
 	std::mutex& g_processMutex;
 	std::atomic<bool>& g_cancelRequested;
 	ProcessRunGuardResult _current;
+
+	void CloseHandlers() {
+		if (_current.pi.hProcess && !closeHProcess) {
+			CloseHandle(_current.pi.hProcess);
+			closeHProcess = true;
+		}
+		if (_current.pi.hThread && !closeHThread) {
+			CloseHandle(_current.pi.hThread);
+			closeHThread = true;
+		}
+		if (_current.out_read && !closeOutRead) {
+			CloseHandle(_current.out_read);
+			closeOutRead = true;
+		}
+		if (_current.err_read && !closeErrRead) {
+			CloseHandle(_current.err_read);
+			closeErrRead = true;
+		}
+	}
 };
